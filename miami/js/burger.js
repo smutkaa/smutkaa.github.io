b6a5022c3acf4167a422263430956b79318562c6
Sabina <sabinsafiullova.fm@mail.ru>
2021-06-30 15:32:05 +0400

    adap rooms234

--- miami/js/burger.js
+++ miami/js/burger.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
 	$('.burger__header').click(function(event){
-		$('.header__burger ,.header__nav, .burger__header, .nav__link, .main__header, .header__menu').toggleClass('active')
+		$('.header__burger ,.header__nav, .header__menu, .burger__header, .nav__link, .rooms__menu, .tworoom__menu, .threeroom__menu, .fourroom__menu').toggleClass('active')
 	});
 });
