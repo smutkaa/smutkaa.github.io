8a457a2790eef493743cb818d2291b15e44dac5c
smutkaa <sabinasafiullova.fm@mail.ru>
2021-09-21 16:19:10 +0400

    Норвегия

---
+++ kmb_fourth/js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+
+	$('.reviews__slaider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots:false
+	});
+
+});
+
---
+++ kmb_fourth/js/scroll.js
@@ -0,0 +1,5 @@
+(function($) {
+    $(window).on('load', function() {
+        $('el').mCustomScrollbar();
+    });
+})(jQuery);
